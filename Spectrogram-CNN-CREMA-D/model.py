f38ffc0682bcc8ec48b839ac251157b4e49dcea8
NorangBerry <hlnsud12@naver.com>
2021-07-16 01:04:50 +0900

    [FEAT] update model

--- Spectrogram-CNN-CREMA-D/model.py
+++ Spectrogram-CNN-CREMA-D/model.py
@@ -8,32 +8,40 @@ class CNN_LSTM(nn.Module):
         super(CNN_LSTM,self).__init__()
         self._build()
 
+    def initialize_weights(self,layer=None):
+        layers = self.children()
+        if layer != None:
+            layers = layer.children()
+        for layer in layers:
+            if type(layer) in [nn.Sequential]:
+                self.initialize_weights(layer)
+            elif hasattr(layer, 'weight'):
+                nn.init.xavier_uniform(layer.weight)
     def _build(self):
         self.CNN = nn.Sequential(
             Conv2d(1, 16, (12,16), padding="same"),
-            nn.ReLU(),
+            # nn.ReLU(),
             MaxPool2d(2,2),
 
             Conv2d(16, 24, (8,12), padding="same"),
-            nn.ReLU(),
+            # nn.ReLU(),
             MaxPool2d(2,2),
 
             Conv2d(24, 32, (5,7), padding="same"),
-            nn.ReLU(),
+            # nn.ReLU(),
             MaxPool2d(2,2)
         )
-        self.LSTM = LSTM(input_size=25*32,hidden_size=128, bidirectional=True)
+        self.LSTM = LSTM(input_size=50*32,hidden_size=128, bidirectional=True)
         # First fully connected layer
         self.dense = nn.Linear(37*256, 64)
-        self.dropout = nn.Dropout(0.5)
-        self.activate = nn.Sequential(
-            nn.Linear(64, 4),
-            nn.Softmax(dim=0)
-        )
+        self.dropout = nn.Dropout(0.3)
+        self.activate = nn.Linear(64, 4)
 
     def forward(self,input):
         cnn = self.CNN(input)
-        cnn = th.flatten(cnn.transpose(1,2), 1,2).transpose(1,2)
+        #(batch, feature, frequency, time)
+        cnn = th.flatten(cnn, 1,2).transpose(1,2)
+        #(batch, time, feature * frequency)
         lstm = self.LSTM(cnn)[0]
         dense = self.dense(th.flatten(lstm,1))
         dropout = self.dropout(dense)
